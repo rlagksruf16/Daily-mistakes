7d3aad2bca991c4bdcfacae60c7b2834a688341c
hankyul <rlagksruf16@gmail.com>
2020-02-12 02:00:21 +0900

    add calendar UI to project

--- lib/pages/calendarPage.dart
+++ lib/pages/calendarPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:table_calendar/table_calendar.dart';
+import 'package:daily_mistakes/pages/mistakeRegisterPage.dart';
 
 
 
@@ -12,10 +13,54 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+
+  CalendarController _calendarController;
+
+  @override
+  void initState() {
+    super.initState();
+    _calendarController = CalendarController();
+  }
+
+  @override
+  void dispose() {
+    _calendarController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      body:
+      TableCalendar(calendarController: _calendarController,),
       
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.pushNamed(context, RegistrationScreen.id);
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 4.0,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.show_chart, size: 30.0, color: Colors.grey),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.calendar_today, size: 30.0, color: Colors.grey),
+              onPressed: () {
+                Navigator.pushNamed(context, CalendarPage.id);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
