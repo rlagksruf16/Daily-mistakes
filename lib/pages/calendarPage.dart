a7c4e11b48ba3ee13e70c71314fe42392d50413e
hankyul <rlagksruf16@gmail.com>
2020-02-12 14:23:08 +0900

    edit calendar page

--- lib/pages/calendarPage.dart
+++ lib/pages/calendarPage.dart
@@ -1,3 +1,4 @@
+import 'package:daily_mistakes/pages/mainPage.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:daily_mistakes/pages/mistakeRegisterPage.dart';
@@ -31,14 +32,43 @@ class _CalendarPageState extends State<CalendarPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-      TableCalendar(calendarController: _calendarController,),
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(left: 20.0),
+                    margin: EdgeInsets.all(10.0),
+                    child: Text(
+                      'Calendar',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30.0,
+                        fontFamily: 'DoHyeon',
+                        fontWeight: FontWeight.bold,
+                        ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ],
+              ),
+              TableCalendar(calendarController: _calendarController,),
+            ],
+          ),
+        ),
+      ),
+      
+      // TableCalendar(calendarController: _calendarController,),
       
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.home),
         onPressed: () {
-          Navigator.pushNamed(context, RegistrationScreen.id);
+          Navigator.pushNamed(context, MainPage.id);
         },
       ),
       bottomNavigationBar: BottomAppBar(
