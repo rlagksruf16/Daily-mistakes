7696994875f809a14d28599917cba35b159869ef
yeeun <ykoou@naver.com>
2020-02-12 16:29:51 +0900

    registerPage complete

--- lib/pages/mistakeRegisterPage.dart
+++ lib/pages/mistakeRegisterPage.dart
@@ -1,6 +1,6 @@
 import 'package:daily_mistakes/pages/mainPage.dart';
 import 'package:flutter/material.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
+import 'calendarPage.dart';
 
 enum Colours {
   pink,
@@ -13,7 +13,9 @@ enum Colours {
   purple,
   white,
 }
+
 Colours selectedButton = Colours.white;
+String mistakeAlert;
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
@@ -23,71 +25,88 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
-  // Colours selectedButton = Colours.white;
+  String mistakeName;
+  String mistakeColor;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: Container(
-        padding: EdgeInsets.only(top: 100.0),
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Text(
-                '실수 등록',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30.0,
-                  fontFamily: 'DoHyeon',
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                '실수 이름 입력',
-                style: TextStyle(
-                  fontFamily: 'DoHyeon',
-                  fontSize: 15.0,
-                ),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                textAlign: TextAlign.center,
-                onChanged: (value) {},
-                decoration: kTextFieldDecoration.copyWith(hintText: '실수 이름 입력'),
-              ),
-              SizedBox(
-                height: 28.0,
-              ),
-              Text(
-                '알람 주기 설정',
-                style: TextStyle(
-                  fontFamily: 'DoHyeon',
-                  fontSize: 15.0,
-                ),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              DropeddownButton(),
-              SizedBox(
-                height: 28.0,
-              ),
-              Text(
-                '실수 색상 설정',
-                style: TextStyle(
-                  fontFamily: 'DoHyeon',
-                  fontSize: 15.0,
-                ),
-              ),
-              SizedBox(
-                height: 8.0,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(left: 20.0),
+                    margin: EdgeInsets.all(10.0),
+                    child: Text(
+                      '실수 등록',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30.0,
+                        fontFamily: 'DoHyeon',
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ],
               ),
               Container(
+                margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                child: Text(
+                  '실수 이름 입력',
+                  style: TextStyle(
+                    fontFamily: 'DoHyeon',
+                    fontSize: 15.0,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(20, 0, 20, 10),
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    mistakeName = value;
+                  },
+                  decoration: InputDecoration(
+                    labelText: "실수 이름을 입력하세요",
+                    labelStyle: TextStyle(
+                        fontFamily: 'DoHyeon',
+                        fontSize: 15.0,
+                        color: Colors.grey),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                child: Text(
+                  '알람 주기 설정',
+                  style: TextStyle(
+                    fontFamily: 'DoHyeon',
+                    fontSize: 15.0,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(20, 0, 20, 10),
+                child: DropeddownButton(),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: Text(
+                  '실수 색상 설정',
+                  style: TextStyle(
+                    fontFamily: 'DoHyeon',
+                    fontSize: 15.0,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
                 child: Column(
                   children: <Widget>[
                     Row(
@@ -181,44 +200,54 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                   ],
                 ),
               ),
-              SizedBox(
-                height: 30.0,
+              Container(
+                margin: EdgeInsets.fromLTRB(20, 30, 20, 10),
+                child: RoundedButton(
+                  title: '실수 등록',
+                  colour: Colors.grey[350],
+                  onPressed: () {
+                    mistakeColor = selectedButton.toString();
+                    print(mistakeName); //print부분 다 지워도 됨.
+                    print(mistakeAlert);
+                    print(mistakeColor);
+                  },
+                ),
               ),
-              RoundedButton(
-                title: '실수 등록',
-                colour: Colors.grey[350],
-                onPressed: () {
-                  _popUpScreen(context); //팝업 테스트 용
-                },
-              )
             ],
           ),
+        ),
+      ),
+      // ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.home),
+        onPressed: () {
+          Navigator.pushNamed(context, MainPage.id);
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 4.0,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.show_chart, size: 30.0, color: Colors.grey),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.calendar_today, size: 30.0, color: Colors.grey),
+              onPressed: () {
+                Navigator.pushNamed(context, CalendarPage.id);
+              },
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-const kTextFieldDecoration = InputDecoration(
-  hintText: 'Enter a value',
-  contentPadding: EdgeInsets.symmetric(vertical: 10.0),
-  // border: OutlineInputBorder(
-  //   borderRadius: BorderRadius.all(Radius.circular(32.0)),
-  // ),
-  enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.grey, width: 1.0),
-    borderRadius: BorderRadius.all(Radius.circular(15.0)),
-  ),
-  focusedBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.grey, width: 2.0),
-    borderRadius: BorderRadius.all(Radius.circular(15.0)),
-  ),
-  hintStyle: TextStyle(
-    fontFamily: 'DoHyeon',
-    fontSize: 15.0,
-    color: Colors.grey,
-  ),
-);
 
 class DropeddownButton extends StatefulWidget {
   // DropeddownButton({@required this.onChanged});
@@ -249,6 +278,7 @@ class _DropeddownButtonState extends State<DropeddownButton> {
         onChanged: (String newValue) {
           setState(() {
             firstvalue = newValue;
+            mistakeAlert = newValue;
           });
         },
         items: <String>[
@@ -291,7 +321,7 @@ class RoundedButton extends StatelessWidget {
         child: Text(
           title,
           style: TextStyle(
-            color: Colors.white,
+            color: Colors.black,
             fontFamily: 'DoHyeon',
             fontSize: 20.0,
           ),
@@ -315,169 +345,14 @@ class ColorButton extends StatelessWidget {
     return GestureDetector(
       onTap: onPress,
       child: Container(
-        width: 40.0,
-        height: 40.0,
+        width: 60.0,
+        height: 60.0,
         margin: EdgeInsets.all(8.0),
         decoration: BoxDecoration(
           color: buttonColor,
           borderRadius: BorderRadius.circular(10.0),
         ),
-        // height: kBottomContainerHeight,
       ),
     );
   }
-}
-
-
-_popUpScreen(context) {
-  var alertStyle = AlertStyle(
-    titleStyle: TextStyle(
-      color: Colors.black,
-      fontSize: 30.0,
-      fontFamily: 'DoHyeon',
-      fontWeight: FontWeight.bold,
-    ),
-  );
-
-  Alert(
-      context: context,
-      title: "실수 등록",
-      style: alertStyle,
-      content: 
-       Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              '실수 이름 입력',
-              style: TextStyle(
-                fontFamily: 'DoHyeon',
-                fontSize: 15.0,
-              ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              onChanged: (value) {},
-              decoration: kTextFieldDecoration.copyWith(hintText: '실수 이름 입력'),
-            ),
-            SizedBox(
-              height: 28.0,
-            ),
-            Text(
-              '알람 주기 설정',
-              style: TextStyle(
-                fontFamily: 'DoHyeon',
-                fontSize: 15.0,
-              ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            DropeddownButton(),
-            SizedBox(
-              height: 28.0,
-            ),
-            Text(
-              '실수 색상 설정',
-              style: TextStyle(
-                fontFamily: 'DoHyeon',
-                fontSize: 15.0,
-              ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            Container(
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      ColorButton(
-                        onPress: () {
-                          selectedButton = Colours.pink;
-                        },
-                        buttonColor: selectedButton == Colours.pink
-                            ? Colors.pink[50]
-                            : Colors.pink,
-                      ),
-                      ColorButton(
-                        onPress: () {
-                          selectedButton = Colours.red;
-                        },
-                        buttonColor: selectedButton == Colours.red
-                            ? Colors.red[50]
-                            : Colors.red,
-                      ),
-                      ColorButton(
-                        onPress: () {
-                          selectedButton = Colours.orange;
-                        },
-                        buttonColor: selectedButton == Colours.orange
-                            ? Colors.orange[50]
-                            : Colors.orange,
-                      ),
-                      ColorButton(
-                        onPress: () {
-                          selectedButton = Colours.yellow;
-                        },
-                        buttonColor: selectedButton == Colours.yellow
-                            ? Colors.yellow[50]
-                            : Colors.yellow,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      ColorButton(
-                        onPress: () {
-                          selectedButton = Colours.green;
-                        },
-                        buttonColor: selectedButton == Colours.green
-                            ? Colors.green[50]
-                            : Colors.green,
-                      ),
-                      ColorButton(
-                        onPress: () {
-                          selectedButton = Colours.lightblue;
-                        },
-                        buttonColor: selectedButton == Colours.lightblue
-                            ? Colors.lightBlue[50]
-                            : Colors.lightBlue,
-                      ),
-                      ColorButton(
-                        onPress: () {
-                          selectedButton = Colours.blue;
-                        },
-                        buttonColor: selectedButton == Colours.blue
-                            ? Colors.blueAccent[50]
-                            : Colors.blueAccent,
-                      ),
-                      ColorButton(
-                        onPress: () {
-                          selectedButton = Colours.purple;
-                        },
-                        buttonColor: selectedButton == Colours.purple
-                            ? Colors.purple[50]
-                            : Colors.purple,
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-          ]),
-      buttons: [
-        DialogButton(
-          onPressed: () => Navigator.pop(context),
-          child: Text(
-            "Register",
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-        )
-      ]).show();
 }
