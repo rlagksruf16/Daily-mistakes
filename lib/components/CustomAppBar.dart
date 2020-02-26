916f2673ec149fd52a50e968a57f953cda13bac0
hankyul <rlagksruf16@gmail.com>
2020-02-26 15:21:58 +0900

    fix errors

--- lib/components/CustomAppBar.dart
+++ lib/components/CustomAppBar.dart
@@ -5,8 +5,7 @@ import 'package:daily_mistakes/pages/overcomePage.dart';
 import 'package:daily_mistakes/pages/statisticPage.dart';
 
 class CustomAppBar extends StatelessWidget {
-  
-  int currentTab = 0;
+
 
   @override
   
@@ -16,68 +15,10 @@ class CustomAppBar extends StatelessWidget {
     return BottomAppBar(
       shape: CircularNotchedRectangle(),
       notchMargin: 4.0,
-      child: Container(
-        height: 60.0,
         child: Row(
           // mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            // MaterialButton(
-            //   minWidth: 40,
-            //   onPressed: () {
-            //     currentTab = 0;
-            //     Navigator.pushNamed(context, SettingPage.id);
-            //   },
-            //   child: Column(
-            //     children: <Widget>[
-            //       Icon(Icons.settings, size: 30.0, color: currentTab == 0 ? Colors.blue : Colors.grey,),
-            //       Text('Setting', style: TextStyle(color: currentTab == 0 ? Colors.blue : Colors.grey),),
-            //     ], 
-            //   ),
-            // ),
-            // MaterialButton(
-            //   minWidth: 40,
-            //   onPressed: () {
-            //     currentTab = 1;
-            //     Navigator.pushNamed(context, StatisticPage.id);
-            //   },
-            //   child: Column(
-            //     children: <Widget>[
-            //       Icon(Icons.show_chart, size: 30.0, color: currentTab == 1 ? Colors.blue : Colors.grey,),
-            //       Text('Chart', style: TextStyle(color: currentTab == 1 ? Colors.blue : Colors.grey),),
-            //     ], 
-            //   ),
-            // ),
-            // Container(
-            //   height: 0,
-            //   width: 90.0,
-            // ),
-            // MaterialButton(
-            //   minWidth: 40,
-            //   onPressed: () {
-            //     currentTab = 2;
-            //     Navigator.pushNamed(context, CalendarPage.id);
-            //   },
-            //   child: Column(
-            //     children: <Widget>[
-            //       Icon(Icons.calendar_today, size: 30.0, color: currentTab == 2 ? Colors.blue : Colors.grey,),
-            //       Text('Setting', style: TextStyle(color: currentTab == 2 ? Colors.blue : Colors.grey),),
-            //     ], 
-            //   ),
-            // ),
-            // MaterialButton(
-            //   minWidth: 40,
-            //   onPressed: () {
-            //     currentTab = 3;
-            //     Navigator.pushNamed(context, OvercomePage.id);
-            //   },
-            //   child: Column(
-            //     children: <Widget>[
-            //       Icon(Icons.delete, size: 30.0, color: currentTab == 3 ? Colors.blue : Colors.grey,),
-            //       Text('Setting', style: TextStyle(color: currentTab == 3 ? Colors.blue : Colors.grey),),
-            //     ], 
-            //   ),
-            // ),
             IconButton(
               icon: Icon(Icons.settings, size: 30.0, color: Colors.grey),
               onPressed: () {
@@ -88,7 +29,6 @@ class CustomAppBar extends StatelessWidget {
               icon: Icon(Icons.show_chart, size: 30.0, color: Colors.grey),
               onPressed: () {
                 Navigator.pushNamed(context, StatisticPage.id);
-                currentTab = 1;
               },
             ),
             Container(
@@ -111,7 +51,6 @@ class CustomAppBar extends StatelessWidget {
             ),
           ],
         ),
-      ),
     );
   }
 }
