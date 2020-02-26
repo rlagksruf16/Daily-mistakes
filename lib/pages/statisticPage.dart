67b767ebfb064c7d21bbedff456d4d7451537dfb
hankyul <rlagksruf16@gmail.com>
2020-02-26 16:01:28 +0900

    fix error

--- lib/pages/statisticPage.dart
+++ lib/pages/statisticPage.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+
+
+class statisticPage extends StatefulWidget {
+  @override
+  _statisticPageState createState() => _statisticPageState();
+}
+
+class _statisticPageState extends State<statisticPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
