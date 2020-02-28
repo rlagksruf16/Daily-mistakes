f3e546f091af9a14feb4bccf632b852db0ca57bc
jihyunle2 <ljh3795@naver.com>
2020-02-28 16:13:22 +0900

    color change and add count time list

--- lib/models/mistake.dart
+++ lib/models/mistake.dart
@@ -5,11 +5,18 @@ class Mistake{
   final Color colour;
   final String alertPeriod;
   int count;
+  var countTime;
+  List countTimeList = List();
 
-  Mistake({this.name, this.colour, this.alertPeriod, this.count = 0});
+  Mistake({this.name, this.colour, this.alertPeriod='하루에 1번', this.count = 0, this.countTime});
+
+  void firstMistakeTime(){
+    countTimeList.add(countTime);
+  }
 
   void countUp(){
     count += 1;
+    countTimeList.add(countTime);
   }
 
 }
