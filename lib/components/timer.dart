6ff7e78a9cce64c22b2341af17c9ee65c48afabf
jihyunle2 <ljh3795@naver.com>
2020-03-09 21:37:31 +0900

    add timer in widget mainPage

--- lib/components/timer.dart
+++ lib/components/timer.dart
@@ -5,13 +5,14 @@ import 'package:daily_mistakes/models/mistake.dart';
 int i=0;
 
 void startTimer(List<Mistake> mistakes, Function moveMistake) {
-  Timer timer = new Timer.periodic(new Duration(seconds: 300), (time) {
+  Timer timer = new Timer.periodic(Duration(seconds: 300), (time) {
     for (var mistake in mistakes) {
       if (mistake.count > 0) {
         Duration difference = mistake.countTimeList[mistake.count]
             .difference(mistake.countTimeList[mistake.count - 1]);
         if (difference.inMinutes >= 1) {
           print(difference.inMinutes);
+
           moveMistake(mistakes.indexOf(mistake));
         }
       }
