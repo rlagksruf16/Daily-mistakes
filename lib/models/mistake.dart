dd06de77676fa33548fa18d810dd0da228a1faac
hankyul <rlagksruf16@gmail.com>
2020-03-21 19:04:28 +0900

    create DB model for SQlite

--- lib/models/mistake.dart
+++ lib/models/mistake.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
 
 class Mistake{
   String name; //실수 카드 수정하려면 final을 지워야 해서 final 삭제.
@@ -22,4 +28,69 @@ class Mistake{
     countTest.add(countTime);
   }
 
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'colour': colour,
+      'alertPeriod': alertPeriod,
+      'count': count,
+      'countTimeList': countTimeList,
+    };
+  }
 }
+
+
+// DB 
+
+final String TableName = 'Mistake';
+
+
+class DBHelper {
+  DBHelper._();
+  static final DBHelper _db = DBHelper._();
+  factory DBHelper() => _db;
+
+  static Database _database;
+
+  Future<Database> get database async {
+    if(_database != null )
+      return _database;
+    
+    _database = await initDB();
+    return _database;
+  }
+
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, 'MistakeDB.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE $TableName(
+            NAME TEXT PRIMARY KEY,
+            COLOUR TEXT,
+            ALERT_PERIOD TEXT,
+            COUNT INT,
+            COUNT_TIME_LIST TEXT,
+          )
+          ''');
+      },
+      onUpgrade: (db, oldVersion, newVersion) {}
+    );
+  }
+
+  //Create
+  createData(Mistake mistake) async {
+    final db = await database;
+    var res = await db.rawInsert('INSERT INTO $TableName(name) VALUES');
+  }
+  //Delete
+
+  //edit
+
+
+}
+
